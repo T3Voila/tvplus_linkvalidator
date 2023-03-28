f10717fa4d74a5bc22bd9184c2dfbbec2b93e903
Alexander Opitz <opitz.alexander@googlemail.com>
2023-03-28 10:11:10 +0200

    Fix TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'TemplaVoilà! Plus - Link Validator',
     'description' => 'Allows the Link Validator to check links inside TemplaVoilà! Plus elements',
     'category' => 'plugin',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Alexander Opitz',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-8.1.99',
-            'typo3' => '8.7.0-11.5.99',
+            'typo3' => '8.7.0-12.4.99',
             'linkvalidator' => '8.7.0-12.4.99',
             'templavoilaplus' => '8.1.0-11.2.99',
         ],
